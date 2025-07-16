e78af2b863aa3359f213146840cf0b236628c711
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-07-16 20:20:34 +0530

    228 updated

--- Array&Strings/Summary_Ranges.py
+++ Array&Strings/Summary_Ranges.py
@@ -5,21 +5,21 @@ Problem: 228: Summary Ranges
 '''
 
 def summaryRanges(nums):
-    result = []
-    if not nums:
-        return result
+    ans = []
+    i = 0
 
-    start = nums[0]
-    for i in range(1, len(nums) + 1):
-        if i == len(nums) or nums[i] != nums[i - 1] + 1:
-            if start == nums[i - 1]:
-                result.append(str(start))
-            else:
-                result.append(f"{start}->{nums[i - 1]}")
-            if i < len(nums):
-                start = nums[i]
-    return result
+    while i < len(nums):
+        start = nums[i]
+        
+        while i < len(nums)-1 and nums[i]+1 == nums[i+1]:
+            i += 1
 
+        if start != nums[i]:
+            ans.append(str(start) + '->' + str(nums[i]))
+        else:
+            ans.append(str(nums[i]))
+        i += 1
+    return ans
 # Example usage and test cases for Summary_Ranges.py
 
 
