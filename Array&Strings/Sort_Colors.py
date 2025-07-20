48203d794b22d7783d904e6b14cb61228b7f1038
HasanMuhammadAli <mali.hasan.612@gmail.com>
2025-07-20 19:23:54 +0530

    75 updated

--- Array&Strings/Sort_Colors.py
+++ Array&Strings/Sort_Colors.py
@@ -7,8 +7,6 @@ def sortColors(nums):
     """
     Sorts the input list nums containing only 0s, 1s, and 2s in-place
     so that all 0s come first, then all 1s, then all 2s.
-
-    
     """
     counts = [0, 0, 0]
     for color in nums:
@@ -26,3 +24,25 @@ print(nums1)  # Output should be: [0, 0, 1, 1, 2, 2]
 nums2 = [1, 0, 1, 2, 0, 2]
 sortColors(nums2)
 print(nums2)  # Output should be: [0, 0, 1, 1, 2, 2]
+
+'''
+def sortColors(nums):
+    counts = {0:0, 1:0, 2:0}
+    for color in nums:
+        counts[color] += 1
+    
+    R, W, B = counts.values()
+    #print(R, W, B)
+
+    nums[:R] = [0] * R
+    nums[R:R+W] = [1] * W
+    nums[R+W:] = [2] * B
+
+nums1 = [2, 0, 2, 1, 1, 0]
+sortColors(nums1)
+print(nums1)  # Output should be: [0, 0, 1, 1, 2, 2]
+
+nums2 = [1, 0, 1, 2, 0, 2]
+sortColors(nums2)
+print(nums2)  # Output should be: [0, 0, 1, 1, 2, 2]
+'''
