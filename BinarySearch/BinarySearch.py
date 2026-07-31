ac95aeff8e8bb807d9fae5117d487eb484b14f54
HasanMuhammadAli <mali.hasan.612@gmail.com>
2026-07-31 18:42:11 +0530

    leetcode 70

---
+++ BinarySearch/BinarySearch.py
@@ -0,0 +1,18 @@
+def search(nums, target):
+    n = len(nums)
+    L = 0
+    R = n-1
+    while L <= R:
+        M = L + ((R-L)//2)
+        if nums[M] == target:
+            return M
+        elif nums[M] < target:
+            L = M +1
+        else:
+            R = M-1
+        
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(search(nums, target))
