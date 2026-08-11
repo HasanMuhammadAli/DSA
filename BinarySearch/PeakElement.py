7a82e790e538b2213de4b9bd8c98e6ed84c573b6
HasanMuhammadAli <mali.hasan.612@gmail.com>
2026-08-11 07:50:49 +0530

    peak element

---
+++ BinarySearch/PeakElement.py
@@ -0,0 +1,13 @@
+def findPeakElement(nums):
+    n = len(nums)
+    if n == 1:
+        return 0
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        mid = left + (right - left) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
